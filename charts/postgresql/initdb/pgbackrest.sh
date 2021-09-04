66999dc53c5180b6f6515297919cdf51ac332e56
Jeffrey C. Ollie <jeff@ocjtech.us>
2021-09-04 15:39:42 -0500

    adapt to switch to rocky linux

--- charts/postgresql/initdb/pgbackrest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-/bin/sed -i -e "s@^#* *archive_command *=.*\$@archive_command = '/usr/bin/pgbackrest archive-push %p'@" /var/lib/postgresql/data/postgresql.conf
-/bin/sed -i -e "s@^#* *archive_mode *=.*\$@archive_mode = on@" /var/lib/postgresql/data/postgresql.conf
-
-/usr/bin/pgbackrest stanza-create
